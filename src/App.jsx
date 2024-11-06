d0df0a0d268aab797477bf2bbd60f5c31f71c12d
Astrid Ge <qiutongg@umich.edu>
2024-11-05 22:26:08 -0800

    complete button3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 // src/App.jsx
 import './App.css';
-// Later on, we'll import button components here
+import Button1 from './components/example-1-normal-css/Button';
+import Button2 from './components/example-2-inline-css/Button';
+import Button3 from "./components/example-3-css-in-js/Button"
+
 
 const App = () => {
   return (
     <div className="App">
       {
-        // Add all example components:
+        <>
+        
         <Button1 buttonText="Example 1" />
-        //...
+        <Button2 buttonText="Example 2" />
+        <Button3 buttonText="Example 3" />
+
+       
+        </>
       }
     </div>
   );
