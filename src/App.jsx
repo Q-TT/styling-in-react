0a52326f0584c814987c25b45c54be8155134400
Astrid Ge <qiutongg@umich.edu>
2024-11-05 22:36:29 -0800

    complete bitton 4

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Button1 from './components/example-1-normal-css/Button';
 import Button2 from './components/example-2-inline-css/Button';
 import Button3 from "./components/example-3-css-in-js/Button"
+import Button4 from './components/example-4-styled-components/Button';
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <Button1 buttonText="Example 1" />
         <Button2 buttonText="Example 2" />
         <Button3 buttonText="Example 3" />
+        <Button4 buttonText="Exmple 4" />
 
        
         </>
--- src/components/example-4-styled-components/Button.jsx
+++ src/components/example-4-styled-components/Button.jsx
@@ -1,6 +1,20 @@
+// src/components/example-4-styled-components/Button.jsx
+import styled from 'styled-components';
+
 const Button4 = ({ buttonText }) => {
-    return <button>{buttonText}</button>;
-  };
+  const StyledButton = styled.button`
+    background-color: #86ba8a;
+    border-radius: 8px;
+    color: #3d3d3d;
+    padding: 15px 50px;
+    font-size: 24pt;
+    width: 300px;
+  `;
+  return (
+    <StyledButton>{ buttonText }</StyledButton>
+  );
+};
+
 
 export default Button4;
   
